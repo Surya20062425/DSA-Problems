1363c70fea79588ad2f3d42f425c99a76d8b6b05
suryaprakash <b.7993974026@gmail.com>
2026-07-20 23:57:42 +0530

    Update Revers_word_string.py

--- Revers_word_string.py
+++ Revers_word_string.py
@@ -2,5 +2,9 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         # Split by any whitespace (handles multiple spaces) and filter out empty strings
         words = s.split()
+        #s.split() is used to split the string s 
+
+        
         # Reverse the list and join with single space
         return ' '.join(reversed(words))
+         # join() function combines elements stored in the word without the spaces
