957ec1c5f5672e26ef95c03af6280fe4558b75a9
suryaprakash <b.7993974026@gmail.com>
2026-07-26 11:44:56 +0530

    Create Consecutive_one.py

---
+++ Consecutive_one.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    # created a class 
+    def longestOnes(self, nums, k):
+
+        # created a function 
+
+        # created two variables nums ,k 
+        left, maxLength, zeroCount = 0, 0, 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zeroCount += 1
+            while zeroCount > k:
+                if nums[left] == 0:
+                    zeroCount -= 1
+                left += 1
+            maxLength = max(maxLength, right - left + 1)
+        return maxLength
