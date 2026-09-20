15d1114846cc68bae3f9fee9ac9ca67e82f4f8e2
Surya <b.7993974026@gmail.com>
2026-09-20 23:47:44 +0530

    Fix Arrow_.py: handle empty input edge case

--- Arrow_.py
+++ Arrow_.py
@@ -1,14 +1,20 @@
-class Solution(object):
-    def findMinArrowShots(self, points):
+from typing import List
+
+class Solution:
+    """Minimum arrows to burst all balloons (greedy interval problem)."""
+
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
         points.sort(key=lambda x: x[0])
         arrows = 1
         end = points[0][1]
-        
+
         for balloon in points[1:]:
-            if balloon[0] > end: 
-                arrows += 1  
-                end = balloon[1] 
+            if balloon[0] > end:
+                arrows += 1
+                end = balloon[1]
             else:
                 end = min(end, balloon[1])
-        
+
         return arrows
