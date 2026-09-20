3bd004b417c2a099dcaaa1a7228a588699af293c
Surya <b.7993974026@gmail.com>
2026-09-20 23:47:14 +0530

    Fix guess number edge case: replace walrus with explicit loop

--- Gues_number.py
+++ Gues_number.py
@@ -1,7 +1,15 @@
+from typing import List
+
 class Solution:
     def guessNumber(self, n: int) -> int:
         low, high = 1, n
-        while (res := guess(mid := ((high + low) >> 1))) != 0:
-            if res == -1: high = mid - 1
-            else: low = mid + 1
-        return mid
+        while low <= high:
+            mid = (low + high) // 2
+            res = guess(mid)
+            if res == 0:
+                return mid
+            elif res == -1:
+                high = mid - 1
+            else:
+                low = mid + 1
+        return low
